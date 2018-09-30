2f5a0f7496b9d7df18585d6f5ab8347edc6b767d
张庭岑 <ztc01@baidu.com>
2018-09-30 14:15:01 +0800

    docs: add some comments

--- plopfile.js
+++ plopfile.js
@@ -1,3 +1,11 @@
+/**
+ * Description.
+ *  A simple plop template for node package.
+ *
+ * @file   This a plopfile.
+ * @author teeeemoji.
+ * @since 2018/9/30
+ */
 const setHelpers = require('./lib/set-partials')
 const setPartials = require('./lib/set-partials')
 const setPrompters = require('./lib/set-prompters')
