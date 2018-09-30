9dbcb04f6c5611c35ea8b1bebaa0a462d838ddc4
张庭岑 <ztc01@baidu.com>
2018-09-30 14:21:19 +0800

    feat: add jest test in template

---
+++ __test__/main.test.js
@@ -0,0 +1,18 @@
+/**
+ * Summary.
+ *  main test file.
+ *
+ * Description
+ *  This is the main test file
+ *
+ * @file   This main test file for plopfile.
+ * @author teeeemoji.
+ * @since 2018/9/30
+ */
+const plopfile = require('../plopfile')
+
+describe('plopfile', function () {
+  it('should plopfile passed', function (done) {
+    done()
+  })
+})
---
+++ plop-templates/node-package/__test__/main.test.js
@@ -0,0 +1,18 @@
+/**
+ * Summary.
+ *  main test file.
+ *
+ * Description
+ *  This is the main test file
+ *
+ * @file   This main test file for class {{ pkgName }}.
+ * @author teeeemoji.
+ * @since {{> DATE}}
+ */
+const main = require('../main')
+
+describe('main', function () {
+  it('should plopfile passed', function (done) {
+    done()
+  })
+})
